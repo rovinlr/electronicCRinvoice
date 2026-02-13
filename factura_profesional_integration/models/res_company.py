13d9b546971e391cb49f77f3fa68b74cab8df5e5
rovinlr <rovinlr@gmail.com>
2026-02-13 13:04:33 -0600

    Fix missing res_company columns during module upgrade

--- factura_profesional_integration/models/res_company.py
+++ factura_profesional_integration/models/res_company.py
@@ -6,17 +6,25 @@ class ResCompany(models.Model):
 
     fp_hacienda_api_base_url = fields.Char(
         string="Hacienda API Base URL",
+        company_dependent=True,
         default="https://api.comprobanteselectronicos.go.cr",
     )
     fp_hacienda_token_url = fields.Char(
         string="Hacienda OAuth Token URL",
+        company_dependent=True,
         default=(
             "https://idp.comprobanteselectronicos.go.cr/auth/realms/rut/protocol/"
             "openid-connect/token"
         ),
     )
-    fp_hacienda_client_id = fields.Char(string="Hacienda Client ID", default="api-prod")
-    fp_hacienda_username = fields.Char(string="Hacienda Username")
-    fp_hacienda_password = fields.Char(string="Hacienda Password")
-    fp_api_timeout = fields.Integer(string="Hacienda API Timeout (s)", default=30)
-    fp_economic_activity_code = fields.Char(string="Actividad económica por defecto (FE)")
+    fp_hacienda_client_id = fields.Char(
+        string="Hacienda Client ID", company_dependent=True, default="api-prod"
+    )
+    fp_hacienda_username = fields.Char(string="Hacienda Username", company_dependent=True)
+    fp_hacienda_password = fields.Char(string="Hacienda Password", company_dependent=True)
+    fp_api_timeout = fields.Integer(
+        string="Hacienda API Timeout (s)", company_dependent=True, default=30
+    )
+    fp_economic_activity_code = fields.Char(
+        string="Actividad económica por defecto (FE)", company_dependent=True
+    )
