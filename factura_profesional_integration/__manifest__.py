597d54d79e8936ee972b6e6b12dd3a1eda8d65da
rovinlr <rovinlr@gmail.com>
2026-02-16 01:44:12 -0600

    Add module image for app listing

--- factura_profesional_integration/__manifest__.py
+++ factura_profesional_integration/__manifest__.py
@@ -4,6 +4,7 @@
     "version": "19.0.5.0.5",
     "category": "Accounting",
     "license": "LGPL-3",
+    "images": ["static/description/Gemini_Generated_Image_hs6p77hs6p77hs6p.png"],
     "depends": ["account", "product", "uom"],
     "data": [
         "security/ir.model.access.csv",
